67ce41eb6c23cdf1eb3b1075ebf447990b8c3bb4
Mirrovex <mirrovex@wp.pl>
2024-05-18 00:16:34 +0200

    register

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', logout_user, name='logout'),
+    path('register/', register_user, name='register'),
 ]
--- website/views.py
+++ website/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from .forms import SignupForm
+
+
 def home(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +22,26 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
+
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successully registered")
+            return redirect('home')
+        return render(request, 'register.html', {'form': form})
+    else:
+        form = SignupForm()
+        return render(request, 'register.html', {'form': form})
